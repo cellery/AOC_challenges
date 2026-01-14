ec1d1bc347dd034fe574a7e894752664933abe3e
Ellery Cochell <cellery2@gmail.com>
2026-01-13 20:26:26 -0700

    Finally have the remap table working in the point network. Hard part is theoretically over now, just need to add a table to track network sizes

--- 2025_c8/src/aoc_types_pkg.sv
+++ 2025_c8/src/aoc_types_pkg.sv
@@ -2,7 +2,7 @@ package aoc_types_pkg;
 
     parameter DIM_W = `DIM_W;
     parameter NUM_POINTS = `NUM_POINTS;
-    parameter MAX_NTWRKS = NUM_POINTS/2;
+    parameter NUM_CONNS  = `NUM_CONNS;
 
     typedef struct packed {
         logic [((DIM_W+1)*2+2)-1:0]    distance;
@@ -16,20 +16,21 @@ package aoc_types_pkg;
         WR_B,   //010
         MERGE,  //011
         IGNORE, //100
-        LOOKUP  //101
+        LOOKUP, //101
+        UPDATE  //110
     } ntwrk_cmd_id_t;
 
     typedef struct packed {
-        logic                          prev_vld;
-        logic [$clog2(MAX_NTWRKS)-1:0] prev_id;
-        logic                          next_vld;
-        logic [$clog2(MAX_NTWRKS)-1:0] next_id;
-        logic                          valid;
+        logic                         prev_vld;
+        logic [$clog2(NUM_CONNS)-1:0] prev_id;
+        logic                         next_vld;
+        logic [$clog2(NUM_CONNS)-1:0] next_id;
+        logic                         valid;
     } ntwrk_remap_node_t;
 
     typedef struct packed {
-        logic [$clog2(MAX_NTWRKS)-1:0] ntwrkb;
-        logic [$clog2(MAX_NTWRKS)-1:0] ntwrka;
-        ntwrk_cmd_id_t                 cmd;
+        logic [$clog2(NUM_CONNS)-1:0] ntwrkb;
+        logic [$clog2(NUM_CONNS)-1:0] ntwrka;
+        ntwrk_cmd_id_t                cmd;
     } ntwrk_size_cmd_t;
 endpackage
--- 2025_c8/src/point_ntwrk.sv
+++ 2025_c8/src/point_ntwrk.sv
@@ -2,6 +2,7 @@ import aoc_types_pkg::*;
 
 module point_ntwrk #(
     parameter NUM_POINTS = 1000,
+    parameter NUM_CONNS = 1000,
     parameter NUM_NTWRKS = 3
 ) (
     input  logic                             clk,
@@ -18,11 +19,12 @@ module point_ntwrk #(
     output logic                             ntwrk_sz_vld
 );
 
-    logic [$clog2(MAX_NTWRKS)-1:0] max_ntwrk_id;
+    //Points to new network id when we create one
+    logic [$clog2(NUM_CONNS)-1:0] max_ntwrk_id;
 
     //Point -> Network signals 
-    logic [$clog2(MAX_NTWRKS)-1:0] ntwrka_lu_id;
-    logic [$clog2(MAX_NTWRKS)-1:0] ntwrkb_lu_id;
+    logic [$clog2(NUM_CONNS)-1:0]  ntwrka_lu_id;
+    logic [$clog2(NUM_CONNS)-1:0]  ntwrkb_lu_id;
     logic [$clog2(NUM_POINTS)-1:0] pointa_in_r;
     logic [$clog2(NUM_POINTS)-1:0] pointb_in_r;
     logic                          read_in_point;
@@ -30,10 +32,10 @@ module point_ntwrk #(
 
     logic                          pointa_ntwrk_wr_en;
     logic [$clog2(NUM_POINTS)-1:0] pointa_ntwrk_wr_addr;
-    logic [$clog2(MAX_NTWRKS)-1:0] pointa_ntwrk_wr_data;
+    logic [$clog2(NUM_CONNS)-1:0]  pointa_ntwrk_wr_data;
     logic                          pointb_ntwrk_wr_en;
     logic [$clog2(NUM_POINTS)-1:0] pointb_ntwrk_wr_addr;
-    logic [$clog2(MAX_NTWRKS)-1:0] pointb_ntwrk_wr_data;
+    logic [$clog2(NUM_CONNS)-1:0]  pointb_ntwrk_wr_data;
 
 
     //Point -> Network signals
@@ -41,29 +43,37 @@ module point_ntwrk #(
 
     //Network lookup/remap table signals
     logic                          lookupa_active;
-    logic                          lookupa_done;
-    logic                          lookupa_ren;
-    logic [$clog2(MAX_NTWRKS)-1:0] lookupa_raddr;
-    logic [$clog2(MAX_NTWRKS)-1:0] lookupa_rdata;
-    logic [$clog2(MAX_NTWRKS)-1:0] lookupa_ntwrk;
+    logic                          lookupa_en;
+    logic [$clog2(NUM_CONNS)-1:0]  lookupa_raddr;
+    logic [$clog2(NUM_CONNS)-1:0]  lookupa_rdata;
+    logic [$clog2(NUM_CONNS)-1:0]  lookupa_ntwrk;
+    logic                          lookupa_wen;
+    logic [$clog2(NUM_CONNS)-1:0]  lookupa_waddr;
+    logic [$clog2(NUM_CONNS)-1:0]  lookupa_wdata;
+    logic [$clog2(NUM_POINTS)-1:0] lookupa_orig_point;
+    logic [$clog2(NUM_CONNS)-1:0]  lookupa_orig_ntwrk;
 
     logic                          lookupb_active;
-    logic                          lookupb_done;
-    logic                          lookupb_ren;
-    logic [$clog2(MAX_NTWRKS)-1:0] lookupb_raddr;
-    logic [$clog2(MAX_NTWRKS)-1:0] lookupb_rdata;
-    logic [$clog2(MAX_NTWRKS)-1:0] lookupb_ntwrk;
+    logic                          lookupb_en;
+    logic [$clog2(NUM_CONNS)-1:0]  lookupb_raddr;
+    logic [$clog2(NUM_CONNS)-1:0]  lookupb_rdata;
+    logic [$clog2(NUM_CONNS)-1:0]  lookupb_ntwrk;
+    logic                          lookupb_wen;
+    logic [$clog2(NUM_CONNS)-1:0]  lookupb_waddr;
+    logic [$clog2(NUM_CONNS)-1:0]  lookupb_wdata;
+    logic [$clog2(NUM_POINTS)-1:0] lookupb_orig_point;
+    logic [$clog2(NUM_CONNS)-1:0]  lookupb_orig_ntwrk;
 
     logic                          remap_active;
     logic                          remap_done;
     ntwrk_cmd_id_t                 remap_ntwrk_action;
 
-    assign pointa_ntwrk_wr_en   = read_in_point_r && (point_ntwrk_action == NEW || point_ntwrk_action == WR_A);
-    assign pointb_ntwrk_wr_en   = read_in_point_r && (point_ntwrk_action == NEW || point_ntwrk_action == WR_B);
-    assign pointa_ntwrk_wr_addr = pointa_in_r; //For now always the registered read address but may have to be updated later
-    assign pointb_ntwrk_wr_addr = pointb_in_r; //For now always the registered read address but may have to be updated later
-    assign pointa_ntwrk_wr_data = (point_ntwrk_action == WR_A) ? ntwrkb_lu_id : max_ntwrk_id;
-    assign pointb_ntwrk_wr_data = (point_ntwrk_action == WR_B) ? ntwrka_lu_id : max_ntwrk_id;
+    assign pointa_ntwrk_wr_en   = point_ntwrk_action == NEW || point_ntwrk_action == WR_A || remap_ntwrk_action == WR_A || remap_ntwrk_action == MERGE;
+    assign pointb_ntwrk_wr_en   = point_ntwrk_action == NEW || point_ntwrk_action == WR_B || remap_ntwrk_action == WR_B || remap_ntwrk_action == MERGE;
+    assign pointa_ntwrk_wr_addr = (remap_ntwrk_action == WR_A || remap_ntwrk_action == MERGE) ? lookupa_orig_point : pointa_in_r; 
+    assign pointb_ntwrk_wr_addr = (remap_ntwrk_action == WR_B || remap_ntwrk_action == MERGE) ? lookupb_orig_point : pointb_in_r; 
+    assign pointa_ntwrk_wr_data = remap_ntwrk_action == WR_A ? lookupb_ntwrk : ((point_ntwrk_action == WR_A) ? ntwrkb_lu_id : max_ntwrk_id);
+    assign pointb_ntwrk_wr_data = remap_ntwrk_action == WR_B ? lookupa_ntwrk : ((point_ntwrk_action == WR_B) ? ntwrka_lu_id : max_ntwrk_id);
 
     //Ready logic
     assign read_in_point = points_in_vld && points_in_rdy;
@@ -81,7 +91,7 @@ module point_ntwrk #(
 
     mdpram #(
         .DEPTH(NUM_POINTS),
-        .WIDTH($clog2(MAX_NTWRKS)),
+        .WIDTH($clog2(NUM_CONNS)),
         .RD_LAT(1),
         .NUM_RD(2),
         .NUM_WR(2)
@@ -101,7 +111,7 @@ module point_ntwrk #(
     always_ff @(posedge clk) begin
         pointa_in_r          <= pointa_in;
         pointb_in_r          <= pointb_in;
-        max_ntwrk_id         <= point_ntwrk_action == NEW ? max_ntwrk_id+1 : max_ntwrk_id; 
+        max_ntwrk_id         <= (point_ntwrk_action == NEW || remap_ntwrk_action == MERGE) ? max_ntwrk_id+1 : max_ntwrk_id; 
         read_in_point_r      <= read_in_point;
         
         if(!rst_n) begin
@@ -112,11 +122,9 @@ module point_ntwrk #(
     //Based on the network IDs from the Point -> Network LUT we decide on our next action
     always_comb begin
         if(read_in_point_r) begin
-            if ((ntwrka_lu_id == '0) ^ (ntwrkb_lu_id == '0)) begin //add to existing network
-                point_ntwrk_action = (ntwrka_lu_id == '0) ? WR_A : WR_B;
-            end else if((ntwrka_lu_id == '0) && (ntwrkb_lu_id == '0)) begin //Create new network
+            if((ntwrka_lu_id == '0) && (ntwrkb_lu_id == '0)) begin //Create new network
                 point_ntwrk_action = NEW;
-            end else if (ntwrka_lu_id != ntwrkb_lu_id) begin //Look up if we network IDs are remapped before we make decision
+            end else if (ntwrka_lu_id != ntwrkb_lu_id) begin //Look up if our network IDs are remapped before we make decision
                 point_ntwrk_action = LOOKUP;
             end else begin
                 point_ntwrk_action = IGNORE;
@@ -126,76 +134,83 @@ module point_ntwrk #(
         end
     end
 
-    //Keep track of lookup status, a and b can have different lookup times so need to track separate
+    //Keep track of lookup/remap status, a and b can have different lookup times so need to track separate
+    assign lookupa_en     = (point_ntwrk_action == LOOKUP) && ntwrka_lu_id != '0;
+    assign lookupa_active = lookupa_en || lookupa_rdata != '0;
+    assign lookupa_raddr  = (point_ntwrk_action == LOOKUP) ? ntwrka_lu_id : lookupa_rdata;
+
+    assign lookupb_en     = (point_ntwrk_action == LOOKUP) && ntwrkb_lu_id != '0;
+    assign lookupb_active = lookupb_en || lookupb_rdata != '0;
+    assign lookupb_raddr  = (point_ntwrk_action == LOOKUP) ? ntwrkb_lu_id : lookupb_rdata;
+
     always_ff @(posedge clk) begin
-        lookupa_active        <= !lookupa_active ? (point_ntwrk_action == LOOKUP) : !lookupa_done;
-        lookupa_raddr         <= (point_ntwrk_action == LOOKUP) ? ntwrka_lu_id : lookupa_rdata;
-        lookupa_ren           <= (point_ntwrk_action == LOOKUP) || (lookupa_active && !lookupa_done);
-        lookupa_ntwrk         <= (point_ntwrk_action == LOOKUP) ? ntwrka_lu_id : ((lookupa_active && lookupa_rdata != '0) ? lookupa_rdata : lookupa_ntwrk);
+        lookupa_orig_point    <= (point_ntwrk_action == LOOKUP) ? pointa_in_r : lookupa_orig_point;
+        lookupa_orig_ntwrk    <= (point_ntwrk_action == LOOKUP) ? ntwrka_lu_id : lookupa_orig_ntwrk;
+        lookupa_ntwrk         <= (point_ntwrk_action == LOOKUP) ? ntwrka_lu_id : ((lookupa_rdata != '0) ? lookupa_rdata : lookupa_ntwrk);
 
-        lookupb_active        <= !lookupb_active ? (point_ntwrk_action == LOOKUP) : !lookupb_done;
-        lookupb_raddr         <= (point_ntwrk_action == LOOKUP) ? ntwrkb_lu_id : lookupb_rdata;
-        lookupb_ren           <= (point_ntwrk_action == LOOKUP) || (lookupb_active && !lookupb_done);
-        lookupb_ntwrk         <= (point_ntwrk_action == LOOKUP) ? ntwrkb_lu_id : ((lookupb_active && lookupb_rdata != '0) ? lookupb_rdata : lookupb_ntwrk);
-
-        remap_active          <= !remap_active ? (point_ntwrk_action == LOOKUP) : remap_done;
+        lookupb_orig_point    <= (point_ntwrk_action == LOOKUP) ? pointb_in_r : lookupb_orig_point;
+        lookupb_orig_ntwrk    <= (point_ntwrk_action == LOOKUP) ? ntwrkb_lu_id : lookupb_orig_ntwrk;
+        lookupb_ntwrk         <= (point_ntwrk_action == LOOKUP) ? ntwrkb_lu_id : ((lookupb_rdata != '0) ? lookupb_rdata : lookupb_ntwrk);
+ 
+        remap_active          <= !remap_active ? (point_ntwrk_action == LOOKUP) : !remap_done;
         
         if(!rst_n) begin
-            lookupa_active    <= 1'b0;
-            lookupa_raddr     <= '0;
-            lookupa_ren       <= 1'b0;
-            lookupa_ntwrk     <= '0;
+            lookupa_orig_point <= '0;
+            lookupa_orig_ntwrk <= '0;
+            lookupa_ntwrk      <= '0;
 
-            lookupb_active    <= 1'b0;
-            lookupb_raddr     <= '0;
-            lookupb_ren       <= 1'b0;
-            lookupb_ntwrk     <= '0;
+            lookupb_orig_point <= '0;
+            lookupb_orig_ntwrk <= '0;
+            lookupb_ntwrk      <= '0;
 
             remap_active      <= 1'b0;
         end
     end
 
-    //Lookup is done once we read 0 from the remap table
-    assign lookupa_done = lookupa_active && lookupa_rdata == '0;
-    assign lookupb_done = lookupa_active && lookupb_rdata == '0;
-
-    //With final network ids we need to decide on next action based on these newer ids (merge or update pointer -> network LUT)
+    //With final network ids we need to decide on next action based on these newer ids (merge and/or update pointer -> network LUT)
     always_comb begin
         if(remap_active && !lookupa_active && !lookupb_active) begin
-            if (lookupa_ntwrk != lookupb_ntwrk) begin //Look up if we network IDs are remapped before we make decision
+            if ((lookupa_ntwrk == '0) ^ (lookupb_ntwrk == '0)) begin //add to existing network
+                remap_ntwrk_action = (lookupa_ntwrk == '0) ? WR_A : WR_B;
+            end else if (lookupa_ntwrk != lookupb_ntwrk) begin
                 remap_ntwrk_action = MERGE;
             end else begin
-                remap_ntwrk_action = IGNORE;
+                remap_ntwrk_action = UPDATE;
             end
         end else begin
             remap_ntwrk_action = IGNORE;
         end
     end
 
+    //Update remap table if we need to merge and make a new network or if we need to update pointers for existing remaps
+    assign lookupa_wen   = remap_ntwrk_action == MERGE || (remap_ntwrk_action == UPDATE && lookupa_orig_ntwrk != lookupa_ntwrk);
+    assign lookupb_wen   = remap_ntwrk_action == MERGE || (remap_ntwrk_action == UPDATE && lookupb_orig_ntwrk != lookupb_ntwrk);
+    assign lookupa_waddr = remap_ntwrk_action == MERGE ? lookupa_ntwrk : lookupa_orig_ntwrk; 
+    assign lookupb_waddr = remap_ntwrk_action == MERGE ? lookupb_ntwrk : lookupb_orig_ntwrk; 
+    assign lookupa_wdata = remap_ntwrk_action == MERGE ? max_ntwrk_id : lookupa_ntwrk;
+    assign lookupb_wdata = remap_ntwrk_action == MERGE ? max_ntwrk_id : lookupb_ntwrk;
+
+    assign remap_done = remap_active && remap_ntwrk_action != IGNORE;
+
     //Table contains a linked list for network remapping
     //TODO - Collapse linked lists with more than 2 nodes to speed up search time
-    /*
     mdpram #(
-        .DEPTH(NUM_POINTS),
-        .WIDTH($clog2(MAX_NTWRKS)),
+        .DEPTH(NUM_CONNS),
+        .WIDTH($clog2(NUM_CONNS)),
         .RD_LAT(1),
         .NUM_RD(2),
         .NUM_WR(2)
-    ) point_ntwrk_lut (
+    ) ntwrk_remap_lut (
         .clk   (clk),
         .rst_n (rst_n),
 
-        .raddr ({pointa_in, pointb_in}),
-        .ren   ({points_in_vld, points_in_vld}),
-        .rdata ({ntwrka_lu_id, ntwrkb_lu_id}),
+        .raddr ({lookupa_raddr,  lookupb_raddr}),
+        .ren   ({lookupa_active, lookupb_active}),
+        .rdata ({lookupa_rdata,  lookupb_rdata}),
 
-        .waddr ({pointa_ntwrk_wr_addr, pointb_ntwrk_wr_addr}),
-        .wen   ({pointa_ntwrk_wr_en, pointb_ntwrk_wr_en}),
-        .wdata ({pointa_ntwrk_wr_data, pointb_ntwrk_wr_data})
+        .waddr ({lookupa_waddr, lookupb_waddr}),
+        .wen   ({lookupa_wen,   lookupb_wen}),
+        .wdata ({lookupa_wdata, lookupb_wdata})
     );
-    */
-
-
-
 
 endmodule
